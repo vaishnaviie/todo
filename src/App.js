d2c32476342846a8d62542108c1d38cb8b42e55d
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2023-08-31 01:49:15 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import PageTitle from './components/PageTitle';
 import styles from './styles/modules/app.module.scss';
-import AppHeader from './components/AppHeader';
 import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
